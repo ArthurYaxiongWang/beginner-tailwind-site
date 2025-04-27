7a6620cdff784e19640264ee056960b43727fa30
ArthurYaxiongWangRRC <yaxiongwang@rrc.ca>
2025-04-27 13:40:39 -0500

    changing to red

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div class="min-h-screen flex items-center justify-center text-6xl bg-teal-400 text-teal-100">
+    <div class="min-h-screen flex items-center justify-center text-6xl bg-red-400 text-teal-100">
       hello i am a website!
     </div>
   );
