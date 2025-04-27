41ff0b353e6276a164c233ebe3149ec02c108233
ArthurYaxiongWangRRC <yaxiongwang@rrc.ca>
2025-04-27 13:32:32 -0500

    quick styles for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 function App() {
   return (
-    <div class="card">
-      <img
-        src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?w=300&h=300&fit=crop&crop=faceses.unsplash.com/photo-1438761681033-6461ffad8d80?w=300"
-        width="130"
-        height="130"
-      />
-      <div class="content">
-        <h2 className="text-2xl font-bold text-gray-800">Lynn Cool Person</h2>
-        <p className="text-gray-600">Product Engineer</p>
-      </div>
+    <div class="min-h-screen flex items-center justify-center text-6xl bg-teal-400 text-teal-100">
+      hello i am a website!
     </div>
   );
 }
